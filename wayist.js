aed6ea4ef9ce6cac18cc4cc050e25a059e2c8ef2
Jan Alonzo <jmalonzo@taguchimail.com>
2013-12-15 16:37:57 +1100

    Add some loading indicator

--- wayist.js
+++ wayist.js
@@ -22,9 +22,28 @@ var wayist = angular.module('wayist', [])
     // Initialize the data
     var data,
         authors = [],
-        authorContent = [];
+        authorContent = [],
+        spinner = new Spinner({
+          lines: 13, // The number of lines to draw
+          length: 20, // The length of each line
+          width: 10, // The line thickness
+          radius: 30, // The radius of the inner circle
+          corners: 1, // Corner roundness (0..1)
+          rotate: 0, // The rotation offset
+          direction: 1, // 1: clockwise, -1: counterclockwise
+          color: '#000', // #rgb or #rrggbb or array of colors
+          speed: 1, // Rounds per second
+          trail: 60, // Afterglow percentage
+          shadow: false, // Whether to render a shadow
+          hwaccel: false, // Whether to use hardware acceleration
+          className: 'spinner', // The CSS class to assign to the spinner
+          zIndex: 2e9, // The z-index (defaults to 2000000000)
+          top: 'auto', // Top position relative to parent in px
+          left: 'auto' // Left position relative to parent in px
+        }).spin(document.querySelector(".spinner"));
 
     $http.get('/wayist/ttch.json').success(function(response) {
+      spinner.stop();
       data = response[0];
       if (!data) return;
 
@@ -49,6 +68,7 @@ var wayist = angular.module('wayist', [])
       authors = authors.sort();
     }).error(function() {
       // Do something here
+      spinner.stop();
     });
 
     $scope.authors = function() {

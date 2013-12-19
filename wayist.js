451541cc805a152c2f405d4949875c7c93442d9a
Jan Alonzo <jmalonzo@taguchimail.com>
2013-12-20 07:50:43 +1100

    Add spinner directive

--- wayist.js
+++ wayist.js
@@ -17,33 +17,48 @@ var wayist = angular.module('wayist', [])
     };
   });
 
+  wayist.directive('spinner', function() {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        var spinner = new Spinner({
+          lines: 13, // The number of lines to draw
+          length: 15, // The length of each line
+          width: 6, // The line thickness
+          radius: 10, // The radius of the inner circle
+          corners: 1, // Corner roundness (0..1)
+          rotate: 0, // The rotation offset
+          direction: 1, // 1: clockwise, -1: counterclockwise
+          color: '#aaa', // #rgb or #rrggbb or array of colors
+          speed: 1, // Rounds per second
+          trail: 60, // Afterglow percentage
+          shadow: false, // Whether to render a shadow
+          hwaccel: true, // Whether to use hardware acceleration
+          className: 'spinner', // The CSS class to assign to the spinner
+          zIndex: 2e9, // The z-index (defaults to 2000000000)
+          top: 'auto', // Top position relative to parent in px
+          left: 'auto' // Left position relative to parent in px
+        });
+
+        spinner.spin(element.get(0));
+
+        scope.$on("dataload.done", function() {
+          spinner.stop();
+        });
+      }
+    };
+  });
+
   wayist.controller('AppController', ["$scope", "$http", "$location", "$anchorScroll", function($scope, $http, $location, $anchorScroll) {
 
     // Initialize the data
     var data,
         authors = [],
-        authorContent = [],
-        spinner = new Spinner({
-          lines: 13, // The number of lines to draw
-          length: 20, // The length of each line
-          width: 10, // The line thickness
-          radius: 30, // The radius of the inner circle
-          corners: 1, // Corner roundness (0..1)
-          rotate: 0, // The rotation offset
-          direction: 1, // 1: clockwise, -1: counterclockwise
-          color: '#000', // #rgb or #rrggbb or array of colors
-          speed: 1, // Rounds per second
-          trail: 60, // Afterglow percentage
-          shadow: false, // Whether to render a shadow
-          hwaccel: false, // Whether to use hardware acceleration
-          className: 'spinner', // The CSS class to assign to the spinner
-          zIndex: 2e9, // The z-index (defaults to 2000000000)
-          top: 'auto', // Top position relative to parent in px
-          left: 'auto' // Left position relative to parent in px
-        }).spin(document.querySelector(".spinner"));
+        authorContent = [];
 
     $http.get('/wayist/ttch.json').success(function(response) {
-      spinner.stop();
+      $scope.$broadcast("dataload.done");
+
       data = response[0];
       if (!data) return;
 

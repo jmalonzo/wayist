00b5ac18aed07d5c7b6c1e0fef61df8faeb636f3
Jan Alonzo <jmalonzo@taguchimail.com>
2014-01-07 20:43:08 +1100

    Add filter test and complete author controller test

--- test/wayist.spec.js
+++ test/wayist.spec.js
@@ -2,8 +2,15 @@ describe("wayist", function() {
   // Initialize module beforee each and every test
   beforeEach(module('wayist'));
 
+  describe("capitalize", function() {
+    it("should be able to capitalize author names", inject(function(capitalizeFilter) {
+      expect(capitalizeFilter("mitchell")).toBe("Mitchell");
+      expect(capitalizeFilter("legge")).toBe("Legge");
+    }));
+  });
+
   describe("AuthorController", function() {
-    var $httpBackend, $rootScope, scope;
+    var $httpBackend, $rootScope, scope, origLocalStorage;
     beforeEach(inject(function(_$rootScope_, _$httpBackend_, $controller) {
       $httpBackend = _$httpBackend_;
       $rootScope = _$rootScope_;
@@ -11,7 +18,27 @@ describe("wayist", function() {
       $controller("AuthorController", {
         $scope: scope
       });
+
+      origLocalStorage = window.localStorage;
+      var store = Object.create(null);
+      store.getItem = function(key) {
+        return store[key];
+      };
+      store.setItem = function(key, value) {
+        store[key] = value + '';
+      };
+      store.clear = function() {
+        store = Object.create(null);
+      };
+      Object.defineProperty(window, "localStorage", {value: store, writeable: true, configurable: true, enumerable: true});
+      spyOn(window.localStorage, 'getItem').andCallThrough();
+      spyOn(window.localStorage, 'setItem').andCallThrough();
     }));
+
+    afterEach(inject(function() {
+      Object.defineProperty(window, "localStorage", {value: origLocalStorage, writeable: true, configurable: true, enumerable: true});
+    }));
+
 
     it("should be able to return the list of authors", function() {
       $httpBackend.expect("GET", "/wayist/data/authors.json")
@@ -22,7 +49,13 @@ describe("wayist", function() {
       $httpBackend.flush();
       expect(scope.authorList).toEqual(["beck", "blackney", "bynner", "mitchell", "wu"]);
     });
-    
+
+    it("should be able to return copyright text", function() {
+      expect(scope.selectedAuthor()).toEqual("by their respective authors.");
+      expect(window.localStorage.getItem).toHaveBeenCalled();
+      window.localStorage.setItem('author', 'Wu');
+      expect(scope.selectedAuthor()).toEqual('Wu');
+      expect(window.localStorage.getItem).toHaveBeenCalled();
+    });
   });
-  
 });
